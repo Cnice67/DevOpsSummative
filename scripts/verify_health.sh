350ed63f8e4b671f74e126507bb644aa17e6d0a4
Cinny Tsado <cinny.tsado@westfieldhealth.com>
2025-05-12 00:18:43 +0100

    fix

--- scripts/verify_health.sh
+++ scripts/verify_health.sh
@@ -1,9 +1,9 @@
-#!/bin/bash
-# wait up to 60s for your Spring-Boot app to answer on 8080
-for i in {1..12}; do
-  if curl --silent http://localhost:8080/actuator/health | grep UP; then
-    exit 0
-  fi
-  sleep 5
-done
-exit 1
+# #!/bin/bash
+# # wait up to 60s for your Spring-Boot app to answer on 8080
+# for i in {1..12}; do
+#   if curl --silent http://localhost:8080/actuator/health | grep UP; then
+#     exit 0
+#   fi
+#   sleep 5
+# done
+# exit 1
