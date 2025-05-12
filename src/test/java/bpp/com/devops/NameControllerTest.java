9aa6ee3a78103c17f7ded09229635a42830ad6c9
Cinny Tsado <cinny.tsado@westfieldhealth.com>
2025-05-12 01:26:09 +0100

    fix

---
+++ src/test/java/bpp/com/devops/NameControllerTest.java
@@ -0,0 +1,25 @@
+package bpp.com.devops;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import bpp.com.devops.controllers.NameController;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+@WebMvcTest(NameController.class)
+class NameControllerTest {
+
+  @Autowired
+  private MockMvc mockMvc;
+
+  @Test
+  @DisplayName("GET /api/name → 200 OK")
+  void getNameEndpointReturns200() throws Exception {
+    mockMvc.perform(get("/api/name"))
+           .andExpect(status().isOk());
+  }
+}
