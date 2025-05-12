4025c40127e18e60f07464e2679633d33dee814a
Cinny Tsado <cinny.tsado@westfieldhealth.com>
2025-05-12 23:14:54 +0100

    Fail case test.

--- src/test/java/bpp/com/devops/controllers/NameController.java
+++ src/test/java/bpp/com/devops/controllers/NameController.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class NameController {
+public class TestController {
 
     @GetMapping("/api/name")
     public String getName() {
